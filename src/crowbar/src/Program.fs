b6ac5afe8eb89d60eed1504e19249abeb0561a93
mxtao <taomengxu@126.com>
2020-03-02 21:31:14 +0800

    remove pointer usage to runtime.fs

--- src/crowbar/src/Program.fs
+++ src/crowbar/src/Program.fs
@@ -1,27 +1,6 @@
 ﻿
 open System
-open System.Runtime.InteropServices
-open Microsoft.FSharp.NativeInterop
 
-// https://docs.microsoft.com/en-us/dotnet/fsharp/whats-new/fsharp-45
-
-#nowarn "9"
-
-let opSpan (span: Span<byte>) = 
-    let s = Span<byte>()
-    span.CopyTo(s)
-    s
-
-let hglobal = Marshal.AllocHGlobal(1024)
-
-let np0 = hglobal.ToPointer() |> NativePtr.ofVoidPtr<byte>
-let np = hglobal |> NativePtr.ofNativeInt<byte>
-let arr = Array.zeroCreate<int>(10)
-let si = NativePtr.stackalloc<int> 1
-let sb = si |> NativePtr.toVoidPtr |> NativePtr.ofVoidPtr<byte>
-
-// must free memory here!
-Marshal.FreeHGlobal(hglobal)
 
 [<EntryPoint>]
 let main argv =
--- src/crowbar/src/Runtime.fs
+++ src/crowbar/src/Runtime.fs
@@ -1,1 +1,28 @@
+// this module for runtime impl
+
 module Corwbar.Runtime
+
+open System
+open System.Runtime.InteropServices
+open Microsoft.FSharp.NativeInterop
+
+#nowarn "9" // disable warnings for using NativePtr module
+
+// https://docs.microsoft.com/en-us/dotnet/fsharp/whats-new/fsharp-45
+
+let operatingSpan (span: Span<byte>) = 
+    let s = Span<byte>()
+    span.CopyTo(s)
+    // ....
+    ()
+
+let ``some operatings`` () = 
+    let hglobal = Marshal.AllocHGlobal(1024)
+    let np0 = hglobal.ToPointer() |> NativePtr.ofVoidPtr<byte>
+    let np = hglobal |> NativePtr.ofNativeInt<byte>
+    let arr = Array.zeroCreate<int>(10)
+    let si = NativePtr.stackalloc<int> 1
+    let sb = si |> NativePtr.toVoidPtr |> NativePtr.ofVoidPtr<byte>
+    // must free memory here!
+    Marshal.FreeHGlobal(hglobal)
+    ()
